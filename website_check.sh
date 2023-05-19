d5c4c5bb5e8445978007de376f51e413d346a82c
xkakac00 <133955012+xkakac00@users.noreply.github.com>
2023-05-19 12:53:03 +0200

    Add files via upload

---
+++ website_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+website="https://www.seznam.cz/"
+
+while true;do
+        # dostupnost musi byt dana do smycky while, aby byla aktualizovana
+	check_available=$(curl -s  --head --fail "$website" > /dev/null | echo $?)
+	if [ "$check_available" -eq 0 ];then
+	   echo -e "\e[32m$website is available...\e[0m"
+	else
+	   echo -e "\e[31m$website is not available...\e[0m"
+	fi
+	sleep 300s	
+done
